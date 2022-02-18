e22e2f32d9681cc963426be43e9d7904b781f691
James <james.dodd@airangel.com>
2022-02-18 12:10:47 +0000

    Routing basics module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent},
+  { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+  { path: 'welcome', component: PageNotFoundComponent},
+];
 
 @NgModule({
+  // RouterModule.forRoot()
+  // declares the router directives
+  // manages our route configuration
+  // registers the router service
+  // used once for the application
+
+  // It is the corollary of RouterModule.forChild()
+  // which also declares the router directives and manages route configuration 
+  // but does not register the router service and is usually used in feature modules
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
